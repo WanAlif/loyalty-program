02b2a08eced08e882b226318b31ba6e4919ed3b4
wan <wanalifnorazlan@gmail.com>
2026-09-09 13:26:44 +0800

    add voucher retrieval endpoint

--- server/src/routes/voucherRoutes.ts
+++ server/src/routes/voucherRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth';
-import { listMyVouchers } from '../controllers/voucherController';
+import { listMyVouchers, redeemVoucher } from '../controllers/voucherController';
 
 const router = Router();
 
 router.use(requireAuth);
 router.get('/me', listMyVouchers);
+router.post('/:id/redeem', redeemVoucher);
 
 export default router;
